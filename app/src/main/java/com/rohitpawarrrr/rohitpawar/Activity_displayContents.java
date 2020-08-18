97ed75ff76b9fb4a6d66e3db3766006f75dd6f03
Rohit Pawar <rohit.pawar20@yahoo.com>
2020-08-18 11:47:18 +0530

    Added Activity_displayContents

---
+++ app/src/main/java/com/rohitpawarrrr/rohitpawar/Activity_displayContents.java
@@ -0,0 +1,16 @@
+package com.rohitpawarrrr.rohitpawar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_displayContents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_display_contents );
+
+
+    }
+}
