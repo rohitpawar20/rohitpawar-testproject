d4b6e7cd6f0cadd8dce2bb036de4720ea9fa8edf
Rohit Pawar <rohit.pawar20@yahoo.com>
2020-08-18 11:58:29 +0530

    Added code to navigate to next activity after 5 second to next activity

--- app/src/main/java/com/rohitpawarrrr/rohitpawar/MainActivity.java
+++ app/src/main/java/com/rohitpawarrrr/rohitpawar/MainActivity.java
@@ -2,15 +2,35 @@ package com.rohitpawarrrr.rohitpawar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    // set time 5 second for splash-screen
+    private static int SPLASH_TIME_OUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
 
+        new Handler().postDelayed( new Runnable() {
+            /* This thread handles the code to navigate spash-screen to next
+            activity after 5 seconds.
+             */
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(getApplicationContext(), Activity_displayContents.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
 
     }
-}
